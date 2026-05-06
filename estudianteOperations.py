7fde301c742e795263bd3946c56149e585de894b
Simón Acosta <simonacostfc@gmail.com>
2026-05-06 18:47:23 -0500

    [ADD] CRUD completo estudiante

--- estudianteOperations.py
+++ estudianteOperations.py
@@ -1,5 +1,6 @@
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
+from sqlmodel.orm import session
 
 from models import EstudianteBase, EstudianteId, EstudianteUpload
 
@@ -11,3 +12,71 @@ def createEstudiante(estudiante: EstudianteBase, session: Session):
     session.refresh(new_estudiante)
 
     return new_estudiante
+
+def getAllEstudiantes(session: Session):
+    return session.query(EstudianteBase).all()
+
+def get_active_students(session: Session):
+    statement = select(EstudianteId).where(EstudianteId.activo == True)
+    results = session.exec(statement).all()
+    return results
+
+def get_inactive_students(session: Session):
+    statement = select(EstudianteId).where(EstudianteId.activo == False)
+    results = session.exec(statement).all()
+    return results
+
+def find_one_student(id: int, session: Session):
+    try:
+        return session.get_one(EstudianteId, id)
+    except NoResultFound:
+        return None
+
+def update_one_student(id: int, new_estudiante: EstudianteUpload, session: Session):
+    estudiante_db = session.get(EstudianteId, id)
+
+    if not estudiante_db:
+        return None
+
+    try:
+        update_data = new_estudiante.model_dump(exclude_unset=True)
+
+        estudiante_db.sqlmodel_update(update_data)
+
+        session.add(estudiante_db)
+        session.commit()
+        session.refresh(estudiante_db)
+        return estudiante_db
+
+    except Exception as e:
+        return None
+
+
+def delete_student(id: int, session: Session):
+    estudiante_db = session.get(EstudianteId, id)
+
+    if not estudiante_db:
+        return None
+
+    estudiante_db.activo = False
+
+    session.add(estudiante_db)
+    session.commit()
+    session.refresh(estudiante_db)
+
+    return estudiante_db
+
+
+def reactivate_estudiante(id: int, session: Session):
+    estudiante_db = session.get(EstudianteId, id)
+
+    if not estudiante_db:
+        return None
+
+    estudiante_db.activo = True
+
+    session.add(estudiante_db)
+    session.commit()
+    session.refresh(estudiante_db)
+
+    return estudiante_db
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from models import EstudianteBase, EstudianteId, EstudianteUpload
-from estudianteOperations import createEstudiante
+from estudianteOperations import createEstudiante, get_active_students, get_inactive_students, find_one_student, update_one_student, delete_student, reactivate_estudiante
 from sqlmodel import Session
 from sqlmodel.orm import session
 from db import SessionDep, create_all_tables
@@ -15,5 +15,51 @@ def saludo():
 async def create_estudiante(estudiante: EstudianteBase, session: SessionDep):
     return createEstudiante(estudiante, session)
 
+@app.get("/estudiantes", response_model=list[EstudianteId])
+async def show_estudiantes(session: SessionDep):
+    estudiante_activos = get_active_students(session)
+    if not estudiante_activos:
+        raise HTTPException(status_code=404, detail="No estudiantes activos")
+    return estudiante_activos
 
+@app.get("/estudiantesInactivos", response_model=list[EstudianteId])
+async def show_estudiantesInactivos(session: SessionDep):
+    estudiante_inactivos = get_inactive_students(session)
+    if not estudiante_inactivos:
+        raise HTTPException(status_code=404, detail="No estudiantes inactivos")
+    return estudiante_inactivos
+
+@app.get("/estudiante/{id}", response_model=EstudianteId)
+async def show_one_estudiante(id: int, session: SessionDep):
+    estudiante = find_one_student(id, session)
+    if not (estudiante):
+        raise HTTPException(status_code=404, detail=f"{id} Estudiante not found")
+    return estudiante
+
+@app.patch("/estudiante/{id}", response_model=EstudianteId, response_model_exclude={"id", "activo"})
+async def update_estudiante(id: int, estudiante: EstudianteUpload, session: SessionDep):
+    update = update_one_student(id, estudiante, session)
+    if not (update):
+        raise HTTPException(status_code=404, detail=f"{id} Estudiante not found")
+    return update
+
+
+@app.delete("/estudiante/{id}", response_model=EstudianteId)
+async def delete_estudiante(id: int, session: SessionDep):
+    estudiante_eliminado = delete_student(id, session)
+
+    if not estudiante_eliminado:
+        raise HTTPException(status_code=404, detail=f"Estudiante {id} no encontrado")
+
+    return estudiante_eliminado
+
+
+@app.patch("/estudiante/rehabilitar/{id}", response_model=EstudianteId)
+def rehabilitar_estudiante(id: int, session: SessionDep):
+    estudiante = reactivate_estudiante(id, session)
+
+    if not estudiante:
+        raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+
+    return estudiante
 
